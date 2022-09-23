f034659090c743c04e87d2ae34f7db405a6691b8
Phil Neibaur <pneibaur@Phils-MacBook-Pro.local>
2022-09-22 22:26:39 -0600

    displays stock info page

--- src/pages/Stocks.jsx
+++ src/pages/Stocks.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import stocks from "../data/data";
 
 
 const Stocks = (props) => {
-    // const stock = props.allStocks.filter( d => d.symbol === props.match.params.symbol)[0]
+    const foundStock = stocks.filter((stockMatch) => stockMatch.symbol === props.match.params.symbol)[0]
+    // console.log("I found your stock:")
+    // console.log(foundStock)
 
     return (
-        <h1>Stocks Page</h1>
+        <div>
+            <h1>{foundStock.name} Info Page</h1>
+            <ul className="stockInfo">
+                <li><strong>EXCHANGE RATE: </strong><span>{foundStock.change}</span></li>
+                <li><strong>LAST PRICE: </strong><span>{foundStock.lastPrice}</span></li>
+                <li><strong>HIGH: </strong><span>{foundStock.high}</span> | <strong>LOW: </strong><span>{foundStock.low} </span></li>
+            </ul>
+        </div>
     )
 }
 
